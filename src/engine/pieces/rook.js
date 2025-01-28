9fbfeea456f10882f912e3f153d03016fa1f8a89
Faith Martin <faith.martin@softwire.com>
2025-01-28 15:45:59 +0000

    Red-3 - added available moves for rook

--- src/engine/pieces/rook.js
+++ src/engine/pieces/rook.js
@@ -1,4 +1,6 @@
 import Piece from './piece';
+import Square from '../square';
+//import BOARD_SIZE from '../gameSettings';
 
 export default class Rook extends Piece {
     constructor(player) {
@@ -6,6 +8,25 @@ export default class Rook extends Piece {
     }
 
     getAvailableMoves(board) {
-        return new Array(0);
+        let location = board.findPiece(this) //{location.row,location.col}
+        let moves = [];
+        
+        //GameSettings.BOARD_SIZE
+        // 1, 3
+        // "col": 1
+        // "row":
+       // moves.push(Square.at(location.row, location.col[i]))
+
+            for(let i=0; i < 8; i++){
+                if(i!==location.col){
+                    moves.push(Square.at(location.row,i))
+                }
+            
+                if(i!==location.row){
+                    moves.push(Square.at(i,location.col))
+                }
+            }
+
+        return moves;
     }
 }
