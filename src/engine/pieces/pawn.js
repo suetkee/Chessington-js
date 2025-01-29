bfd30b1ff9bfaf41cd977fb6e0ee53599a6ab8ee
Suet Kee <suet.kee@softwire.com>
2025-01-29 15:21:04 +0000

    Red 8 completed

--- src/engine/pieces/pawn.js
+++ src/engine/pieces/pawn.js
@@ -1,6 +1,7 @@
 import Player from '../player';
 import Square from '../square';
 import Piece from './piece';
+//import getPiece from '../board';
 //import { board } from '../../frontend/js/chessington';
 
 export default class Pawn extends Piece {
@@ -9,22 +10,52 @@ export default class Pawn extends Piece {
     }
 
     getAvailableMoves(board) {
-        let location = board.findPiece(this)
-        let location2 = board.findPiece(blockingPiece)
+       let location = board.findPiece(this)
+       //let location_blockingPiece = board.findPiece(board.blockingPiece)
         let moves = [];
+
+
+        //let blockingPiece = getPiece(this.location.row + 1, this.location.col) 
+            //     return this.board[square.row][square.col]
         // add another if to move 2 place 1st go
+// for the pawn location use get piece for square in front
+     //  /* getPiece(square) {
+       //     return this.board[square.row][square.col];
+        //}
+
+        // if get piece != empty return empty array 
+
+
+
         if (this.player === Player.WHITE) {
-            moves.push(Square.at(location.row + 1, location.col))
-            if (location.row === 1){
-                moves.push(Square.at(location.row + 2, location.col))
+           if (location.row === 1){
+                if(board.getPiece(Square.at(location.row +2, location.col))===undefined && board.getPiece(Square.at(location.row +1, location.col))===undefined){
+                    moves.push(Square.at(location.row + 2, location.col))
+                }
             }
+                      
+            if(board.getPiece(Square.at(location.row +1, location.col))===undefined){
+                    moves.push(Square.at(location.row + 1, location.col))
+                }
+
+            } 
+  
+        else {
+            if (location.row == 6){
+                if(board.getPiece(Square.at(location.row -2, location.col))===undefined && board.getPiece(Square.at(location.row -1, location.col))===undefined){
+                    moves.push(Square.at(location.row - 2, location.col))
+                }
+            } 
+
+            if(board.getPiece(Square.at(location.row -1, location.col))===undefined){
+                moves.push(Square.at(location.row - 1, location.col))
+            }
+        } 
+
+           
             
-        } else {
-            moves.push(Square.at(location.row - 1, location.col))
-            if (location.row === 6){
-                moves.push(Square.at(location.row - 2, location.col))
-            }
-        }
+        
         return moves;
     }
+
 }
